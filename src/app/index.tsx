50f745ca05f12fce2086a6a44f6ccfa565a8ab9c
dhoouglas <dhoouglas@hotmail.com>
2025-07-29 10:19:08 -0300

    setup: configure Expo Router with Inter font, global layout, loading screen, and placeholder routes

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,25 @@
-import { Text, View } from "react-native";
+import { Button, Text, View } from "react-native";
+import { router } from "expo-router";
+import { fontFamily } from "@/theme/fontFamily";
 
 export default function Index() {
   return (
     <View style={{ flex: 1, justifyContent: "center" }}>
-      <Text>Olá, expo router</Text>
+      <Text style={{ fontFamily: fontFamily.bold, fontSize: 34 }}>
+        Olá, expo router
+      </Text>
+
+      <Button title="Nova Meta" onPress={() => router.navigate("/target")} />
+
+      <Button
+        title="Transação"
+        onPress={() => router.navigate("/transaction/132")}
+      />
+
+      <Button
+        title="Progresso"
+        onPress={() => router.navigate("/in-progress/12")}
+      />
     </View>
   );
 }
