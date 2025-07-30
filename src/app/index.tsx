523435799d60e40b7a516d51b5e30d996a10f532
dhoouglas <dhoouglas@hotmail.com>
2025-07-30 11:38:42 -0300

    feat: add financial targets section with reusable List and Target components

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,6 @@
 import { HomeHeader } from "@/components/HomeHeader";
+import { List } from "@/components/List";
+import { Target } from "@/components/Target";
 import { View } from "react-native";
 
 const summary = {
@@ -7,10 +9,43 @@ const summary = {
   output: { label: "Saídas", value: "-R$ 883,65" },
 };
 
+const targets = [
+  {
+    id: "1",
+    name: "Apple watch",
+    current: "R$ 580,00",
+    percentage: "50%",
+    target: "R$ 1.790,00",
+  },
+  {
+    id: "2",
+    name: "Comprar uma cadeira ergonômica",
+    current: "R$ 900,00",
+    percentage: "75%",
+    target: "R$ 1.200,00",
+  },
+  {
+    id: "3",
+    name: "Fazer uma viagem para o Rio de Janeiro",
+    current: "R$ 1200,00",
+    percentage: "75%",
+    target: "R$ 3.000,00",
+  },
+];
+
 export default function Index() {
   return (
     <View style={{ flex: 1 }}>
       <HomeHeader data={summary} />
+
+      <List
+        title="Metas"
+        data={[]}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <Target data={item} />}
+        emptyMessage="Nenhuma meta. Toque em nova meta para criar."
+        containerStyle={{ paddingHorizontal: 24 }}
+      />
     </View>
   );
 }
