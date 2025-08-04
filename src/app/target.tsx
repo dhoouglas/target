d36af4532e842d8ec33c17cc650bf3095d0f3e52
dhoouglas <dhoouglas@hotmail.com>
2025-08-04 14:24:57 -0300

    feat: configured sqlite

--- src/app/target.tsx
+++ src/app/target.tsx
@@ -1,32 +1,79 @@
-import { View } from "react-native";
-
-import { PageHeader } from "@/components/PageHeader";
-import { Input } from "@/components/Input";
 import { Button } from "@/components/Button";
-
 import { CurrencyInput } from "@/components/CurrencyInput";
+import { Input } from "@/components/Input";
+import { PageHeader } from "@/components/PageHeader";
+import { useTargetDatabase } from "@/database/useTargetDatabase";
+import { router, useLocalSearchParams } from "expo-router";
+import { useState } from "react";
+import { Alert, View } from "react-native";
 
 export default function Target() {
+  const [isProcessing, setIsProcessing] = useState(false);
+  const [name, setName] = useState("");
+  const [amount, setAmount] = useState(0);
+
+  const params = useLocalSearchParams<{ id?: string }>();
+  const targetDatabase = useTargetDatabase();
+
+  function handleSave() {
+    if (!name.trim() || amount <= 0) {
+      return Alert.alert(
+        "Atenção",
+        "Preencha o nome e o valor precisa ser maior que zero."
+      );
+    }
+
+    setIsProcessing(true);
+
+    if (params.id) {
+      // update
+    } else {
+      create();
+    }
+  }
+
+  async function create() {
+    try {
+      await targetDatabase.create({ name, amount });
+      Alert.alert("Nova Meta", "Meta criada com sucesso!", [
+        {
+          text: "Ok",
+          onPress: router.back,
+        },
+      ]);
+    } catch (error) {
+      Alert.alert("Erro", "Não foi possível criar a meta.");
+      console.log(error);
+      setIsProcessing(false);
+    }
+  }
+
   return (
     <View style={{ flex: 1, padding: 24 }}>
       <PageHeader
         title="Meta"
         subtitle="Economize para alcançar sua meta financeira."
-        // rightButton={{
-        //   icon: "edit",
-        //   onPress: () => {},
-        // }}
       />
 
       <View style={{ marginTop: 32, gap: 24 }}>
         <Input
-          label="Nome da meta"
-          placeholder="Ex: Viagem para pria, Apple Watch"
+          label="Nova meta"
+          placeholder="Ex: Viagem para praia, Apple Watch"
+          onChangeText={setName}
+          value={name}
         />
 
-        <CurrencyInput label="Valor alvo (R$)" value={24300.73} />
+        <CurrencyInput
+          label="Valor alvo (R$)"
+          value={amount}
+          onChangeValue={setAmount}
+        />
 
-        <Button title="Salvar" />
+        <Button
+          title="Salvar"
+          isProcessing={isProcessing}
+          onPress={handleSave}
+        />
       </View>
     </View>
   );
